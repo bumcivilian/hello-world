641724bf1b1494c948c77b4ab71b0ab0ab158ccf
bum <am.dargatz@gmail.com>
2020-02-26 22:51:53 -0500

    Improved request recognition

--- plexbot.js
+++ plexbot.js
@@ -2,50 +2,39 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-let comboMap = new Map(); // map holding {title, user}
+// map holding {title, user}
+let comboMap = new Map(); 
 
 client.on("ready", () => {
     console.log("I am ready!");
 });
 
 client.on("message", (message) => {
+    getUserRequest();
+    alertUserWhenRequestIsComplete();
 
-    // TODO: change to the MellowBot confirmation, which contains the exact title:  "@Tom Flakes, Requested Star Trek: Discovery in Ombi.""
-    // no longer has to check for movie vs. tv requests. uses the mellowbot confirmation to get exact title
+    /* Map contents:
+        comboMap.entries() == { title, user }
+        currentEntry[0] == title
+        currentEntry[1] == user
+    */
+});
+
+function getUserRequest() {
+    // no longer has to check for movie vs. tv requests. uses the Mellowbot confirmation to get exact title
     if (message.author.username == "Mellow" && message.content.includes("Requested")) {
         user = message.content.split(",");   
         user = user[0];
         startIndex = message.content.indexOf(",");
-        title = message.content.slice(startIndex + 12, -8);
+        title = message.content.slice(startIndex + 12, -9);
 
         comboMap.set(title, user);
         console.log(user + " has requested " + title);
         // message.channel.send(user + " has requested " + title);
     }
+}
 
-
-
-
-    // catch the inital request and store the movie title and requesting user
-    // if (message.content.startsWith("request movie")) {
-    //     title = message.content.substr(14, message.content.length);
-    //     user = message.author;
-
-    //     comboMap.set(title.toLowerCase(), user);
-    //     console.log(user.username + " has requested " + title.toLowerCase());
-    //     console.log("umm");
-    // }
-
-
-    // catch the inital request and store the tv title and requesting user
-    // if (message.content.startsWith("request tv")) {
-    //     title = message.content.substr(11, message.content.length);
-    //     user = message.author;
-
-    //     comboMap.set(title.toLowerCase(), user);
-    //     console.log(user.username + " has requested " + title.toLowerCase());
-    // }
-
+function alertUserWhenRequestIsComplete() {
     // if the message is sent by the Plex webhook  
     // iterate through the map entries and check if the Plex message starts with a title in the map
     // if so, get the user who requested that title, then tell them their request is complete
@@ -58,12 +47,6 @@ client.on("message", (message) => {
             }
         }
     }
-
-    /* Map contents:
-        comboMap.entries() == { title, user }
-        currentEntry[0] == title
-        currentEntry[1] == user
-    */
-});
+}
 
 client.login(config.token);
